5503be766e2e3e91afc45e1ef1d86dd3d7af4a84
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-05-24 17:54:14 -0400

    Update the index page to show flight results of search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -3,11 +3,14 @@ class FlightsController < ApplicationController
     @flights = Flight.all
     @flight_codes = Airport.all.map { |f| [f.code, f.id] }
     @flight_dates = @flights.all.map { |f| [f.departure_datetime.to_date, f.id] }
+
+    @available_flights = Flight.where(arrival_airport: flight_params[:arrival_code],
+                                      departure_airport: flight_params[:departure_code])
   end
 
   private
 
   def flight_params
-    params.require(:flight).permit(:arrival_code, :departure_code, :date, :num_tickets)
+    params.permit(:arrival_code, :departure_code, :date, :num_tickets)
   end
 end
