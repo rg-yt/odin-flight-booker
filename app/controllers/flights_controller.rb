86e0153390185bb5f0aa6370e2e8986eed1af342
rg-yt <103154533+rg-yt@users.noreply.github.com>
2024-05-24 16:56:57 -0400

    Update flights controller and form to user rails helpers

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,13 @@
 class FlightsController < ApplicationController
   def index
     @flights = Flight.all
+    @flight_codes = Airport.all.map { |f| [f.code, f.id] }
+    @flight_dates = @flights.all.map { |f| [f.departure_datetime.to_date, f.id] }
+  end
+
+  private
+
+  def flight_params
+    params.require(:flight).permit(:arrival_code, :departure_code, :date, :num_tickets)
   end
 end
